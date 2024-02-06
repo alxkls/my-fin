f3e69e65597767f3956efdb38babaff75bbb8f61
alxkls <oleksikoleso@gmail.com>
2024-02-06 22:00:00 +0000

    feat: enable 'add calls' for core
    For mf-core implemented add user and add transaction endpoints

--- mf-core/src/main/java/com/alxkls/mfcore/svc/CoreService.java
+++ mf-core/src/main/java/com/alxkls/mfcore/svc/CoreService.java
@@ -3,6 +3,8 @@ package com.alxkls.mfcore.svc;
 
 import com.alxkls.mfcore.client.MFTransactionClient;
 import com.alxkls.mfcore.client.MFUserClient;
+import com.alxkls.mfcore.map.MFTransaction;
+import com.alxkls.mfcore.map.MFUser;
 import com.alxkls.mfcore.map.MFUserTransactions;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,7 +18,6 @@ public class CoreService {
 
   public MFUserTransactions getUserTransactions(Long userId) {
     var user = userClient.findUser(userId);
-
     var transactions = transactionClient.getTransactionsByUserId(userId);
 
     return MFUserTransactions.builder()
@@ -25,7 +26,15 @@ public class CoreService {
         .startCapital(user.getStartCapital())
         .transactions(transactions)
         .build();
-
   }
+
+  public void saveUser(MFUser user){
+    userClient.saveUser(user);
+  }
+  public void saveTransaction(MFTransaction transaction){
+    transactionClient.saveTransaction(transaction);
+  }
+
+
 
 }
